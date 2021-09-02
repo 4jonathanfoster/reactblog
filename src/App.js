ad247bdf3c9dff1b48911488e804459429130c31
EinsteinDG <enty84@gmail.com>
2021-09-02 10:59:10 -0500

    adding logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Home from "../src/pages/home/Home";
+// import Home from "../src/pages/home/Home";
 import Topbar from "../src/components/topbar/Topbar";
 // import Single from "../src/pages/single/Single";
+import logout from "./components/logout/logout"
 
 function App() {
   return (
     <>
-    
     <Topbar/>
-    <Single/>
+    <logout/>
 
     </>
     
---
+++ src/components/logout/logout.jsx
@@ -0,0 +1,12 @@
+import "./logout.css";
+
+const logout = () => {
+    return (
+        <div>
+            <p className="logOut">Log-out</p>
+        </div>
+    );
+}
+
+export default logout;
+
