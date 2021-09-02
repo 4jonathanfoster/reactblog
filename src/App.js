87ecefc913d1df6ff3d93a3956d43158ce562716
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-09-02 12:55:47 -0500

    write post page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Home from "../src/pages/home/Home";
 import Topbar from "../src/components/topbar/Topbar";
-// import Single from "../src/pages/single/Single";
-
+import Single from "../src/pages/single/Single";
+import Write from "../src/pages/write/Write"
 function App() {
   return (
     <>
     
     <Topbar/>
-    <Single/>
+    <Write/>
 
     </>
     
