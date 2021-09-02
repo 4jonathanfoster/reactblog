8688bdb4b4a8565bbd91833e7fb60d5e74d3102c
cnvives <cnvives@gmail.com>
2021-09-02 13:28:25 -0500

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Home from "../src/pages/home/Home";
 import Topbar from "../src/components/topbar/Topbar";
 // import Single from "../src/pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings"
 
 function App() {
   return (
     <>
-    
-    <Topbar />
-    <Write />
-    <Single/>
-
+      <Topbar />
+      <Settings />
     </>
     
   );
