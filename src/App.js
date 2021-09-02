6f4247af269108a02b318273c1dff0e0d50704fe
EinsteinDG <enty84@gmail.com>
2021-09-02 12:46:32 -0500

    logout page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // import Home from "../src/pages/home/Home";
 import Topbar from "../src/components/topbar/Topbar";
 // import Single from "../src/pages/single/Single";
-import logout from "./components/logout/logout"
+import Logout from "./components/logout/Logout.jsx"
 
 function App() {
   return (
     <>
     <Topbar/>
-    <logout/>
+    <Logout/>
 
     </>
     
--- src/components/logout/logout.jsx
+++ src/components/logout/logout.jsx
@@ -1,12 +1,16 @@
 import "./logout.css";
 
-const logout = () => {
+export default function Logout() {
     return (
-        <div>
-            <p className="logOut">Log-out</p>
+        <div className="logOut">
+            <div className="logoutSquare">
+
+           <h1 className="logoutTitle">You have been Logged Out</h1>
+          <p className="logMessage">Thanks for using TechBlog!</p>
+          <button className="loginButton">Login Again!</button>
+          </div>
         </div>
+  
     );
 }
-
-export default logout;
 
