8be0f63c2c90de20ac1082a368be6be8ddde3763
EinsteinDG <enty84@gmail.com>
2021-09-03 01:44:48 -0500

    polished header img/title

--- src/components/header2/Header2.jsx
+++ src/components/header2/Header2.jsx
@@ -10,8 +10,12 @@ export default function Header2() {
             </div>
             <img 
             className="header2Img" 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4mdHLpRjpskTb3UXdYVGeVr4PGQGFMQfxkA&usqp=CAU" 
+            src="https://images.pexels.com/photos/5475752/pexels-photo-5475752.jpeg?cs=srgb&dl=pexels-anthony-shkraba-5475752.jpg&fm=jpg" 
             alt=""/>
+            {/* <img 
+            className="header2Img" 
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4mdHLpRjpskTb3UXdYVGeVr4PGQGFMQfxkA&usqp=CAU" 
+            alt=""/> */}
         </div>
     );
 }
